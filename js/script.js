924fd05d95b859409d113993f19f30e1899360cd
Sergey <2sting2@mail.ru>
2020-05-10 18:11:17 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+let q1 = prompt("Один из последних просмотренных фильмов?", ""),
+		q2 = prompt("На сколько оцените его?", ""),
+		q3 = prompt("Один из последних просмотренных фильмов?", ""),
+		q4 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[q1] = q2;
+personalMovieDB.movies[q3] = q4;
